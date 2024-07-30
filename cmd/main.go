79a0f91439f85a778b3551f302e0adec5026c2d7
Rui Wang <wruiwr86@gmail.com>
2024-07-30 13:30:28 +0200

    add go mod and main function for the start od the development

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Start development kube-deploy-automation!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/uib-ub/hono-kube-deploy-automation
+
+go 1.22.2
