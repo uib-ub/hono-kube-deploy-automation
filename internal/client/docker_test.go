96605c81e3931e0175697da57c92fd84598918b8
Rui Wang <wruiwr86@gmail.com>
2024-08-02 10:17:09 +0200

    update docker test by using Table-Driven tests

--- internal/client/docker_test.go
+++ internal/client/docker_test.go
@@ -5,49 +5,64 @@ import (
 	"testing"
 )
 
-var (
-	repoFullName     = "uib-ub/uib-ub-monorepo"
-	registryOwner    = "uib-ub"
-	imageTag         = "test"
-	localRepoSrcPath = os.Getenv("LOCAL_REPO_SRC")
-)
-
-var dockerOptions = DockerOptions{
-	ContainerRegistry: "ghcr.io",
-	RegistryPassword:  os.Getenv("GITHUB_TOKEN"),
-	Dockerfile:        "Dockerfile.api",
-	ImageNameSuffix:   "api",
+// Test cases
+var dockerTestCases = []struct {
+	repoFullName     string
+	registryOwner    string
+	imageTag         string
+	localRepoSrcPath string
+	dockerOptions    *DockerOptions
+}{
+	{
+		repoFullName:     "uib-ub/uib-ub-monorepo",
+		registryOwner:    "uib-ub",
+		imageTag:         "test",
+		localRepoSrcPath: os.Getenv("LOCAL_REPO_SRC"),
+		dockerOptions: &DockerOptions{
+			ContainerRegistry: "ghcr.io",
+			RegistryPassword:  os.Getenv("GITHUB_TOKEN"),
+			Dockerfile:        "Dockerfile.api",
+			ImageNameSuffix:   "api",
+		},
+	},
 }
 
 func TestImageBuild(t *testing.T) {
-	dockerClient, err := NewDockerClient(&dockerOptions)
-	if err != nil {
-		t.Fatalf("failed to create docker client: %v", err)
-	}
-	err = dockerClient.ImageBuild(registryOwner, repoFullName, imageTag, localRepoSrcPath)
-	if err != nil {
-		t.Fatalf("failed to build image: %v", err)
+	for i, tc := range dockerTestCases {
+		dockerClient, err := NewDockerClient(tc.dockerOptions)
+		if err != nil {
+			t.Fatalf("failed to create docker client in test case %d: %v", i, err)
+		}
+
+		err = dockerClient.ImageBuild(tc.registryOwner, tc.repoFullName, tc.imageTag, tc.localRepoSrcPath)
+		if err != nil {
+			t.Fatalf("failed to build image in test case %d: %v", i, err)
+		}
 	}
 }
 
 func TestImagePush(t *testing.T) {
-	dockerClient, err := NewDockerClient(&dockerOptions)
-	if err != nil {
-		t.Fatalf("failed to create docker client: %v", err)
-	}
-	err = dockerClient.ImagePush(registryOwner, repoFullName, imageTag)
-	if err != nil {
-		t.Fatalf("failed to push image: %v", err)
+	for i, tc := range dockerTestCases {
+		dockerClient, err := NewDockerClient(tc.dockerOptions)
+		if err != nil {
+			t.Fatalf("failed to create docker client in test case %d: %v", i, err)
+		}
+		err = dockerClient.ImagePush(tc.registryOwner, tc.repoFullName, tc.imageTag)
+		if err != nil {
+			t.Fatalf("failed to push image in test case %d: %v", i, err)
+		}
 	}
 }
 
 func TestImageDelete(t *testing.T) {
-	dockerClient, err := NewDockerClient(&dockerOptions)
-	if err != nil {
-		t.Fatalf("failed to create docker client: %v", err)
-	}
-	err = dockerClient.ImageDelete(registryOwner, repoFullName, imageTag)
-	if err != nil {
-		t.Fatalf("failed to delete image: %v", err)
+	for i, tc := range dockerTestCases {
+		dockerClient, err := NewDockerClient(tc.dockerOptions)
+		if err != nil {
+			t.Fatalf("failed to create docker client in test case %d: %v", i, err)
+		}
+		err = dockerClient.ImageDelete(tc.registryOwner, tc.repoFullName, tc.imageTag)
+		if err != nil {
+			t.Fatalf("failed to delete image in test case %d: %v", i, err)
+		}
 	}
 }
