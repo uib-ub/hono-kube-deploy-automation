631521fcc673a61067b8a3106e5be7771a19407b
Rui Wang <wruiwr86@gmail.com>
2024-08-08 16:48:28 +0200

    add comments for kustomize code

--- internal/client/kustomize.go
+++ internal/client/kustomize.go
@@ -9,10 +9,13 @@ import (
 	"sigs.k8s.io/kustomize/api/types"
 )
 
+// Kustomizer implements a kustomizer, which is used to build kustomize
+// resources from a given source.
 type Kustomizer struct {
-	KubeSrc string
+	KubeSrc string // KubeSrc is the source directory containing kustomize resources.
 }
 
+// NewKustomizer returns a new instance of Kustomizer with the provided kubeSrc.
 func NewKustomizer(kubeSrc string) *Kustomizer {
 	return &Kustomizer{
 		KubeSrc: kubeSrc,
@@ -20,21 +23,28 @@ func NewKustomizer(kubeSrc string) *Kustomizer {
 }
 
 // Build compiles the kustomize resources into a slice of YAML strings.
+// It returns the compiled YAML strings or an error if the build process fails.
 func (k *Kustomizer) Build() ([]string, error) {
 	log.Infof("Building kustomize resources from %s", k.KubeSrc)
+	// Create a filesystem interface for the kustomize to interact with the disk.
 	fs := filesys.MakeFsOnDisk()
-	opt := &krusty.Options{PluginConfig: types.DisabledPluginConfig()}
-	krust := krusty.MakeKustomizer(opt)
+	// Build compiles the kustomize resources into a slice of YAML strings.
+	opts := &krusty.Options{PluginConfig: types.DisabledPluginConfig()}
+	// Create a kustomizer instance with the options.
+	krust := krusty.MakeKustomizer(opts)
+	// Run the kustomizer to build the resources from the source.
 	res, err := krust.Run(fs, k.KubeSrc)
 	if err != nil {
 		return nil, fmt.Errorf("failed to build kustomize resources: %w", err)
 	}
+	// Initialize a slice to hold the resulting YAML strings.
 	allKubeResources := make([]string, 0, len(res.Resources()))
 	for _, r := range res.Resources() {
 		kubeRes, err := r.AsYAML()
 		if err != nil {
 			return nil, fmt.Errorf("failed to convert kustomize resource to YAML: %w", err)
 		}
+		// Append the YAML string to the result slice.
 		allKubeResources = append(allKubeResources, string(kubeRes))
 	}
 	return allKubeResources, nil
