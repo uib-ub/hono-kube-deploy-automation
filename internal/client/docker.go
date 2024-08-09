53888ee039bebcee5518285604ab6adf77d7514e
Rui Wang <wruiwr86@gmail.com>
2024-08-09 10:54:47 +0200

    add comments for docker code

--- internal/client/docker.go
+++ internal/client/docker.go
@@ -18,17 +18,20 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// DockerOptions is a struct that holds the options for the Docker API client operations.
 type DockerOptions struct {
-	ContainerRegistry string
-	RegistryPassword  string
-	Dockerfile        string
+	ContainerRegistry string // the registry where the image will be pushed
+	RegistryPassword  string // the password for the registry
+	Dockerfile        string // the Dockerfile to use for building the image
 }
 
+// DockerClient warps the Docker API client and the options for Docker operations.
 type DockerClient struct {
-	Client        *dockercli.Client
-	DockerOptions *DockerOptions
+	Client        *dockercli.Client // Client is the Docker API client
+	DockerOptions *DockerOptions    // DockerOptions holds the configuration options for Docker operations.
 }
 
+// NewDockerClient creates a new Docker client with the given options.
 func NewDockerClient(dockerOptions *DockerOptions) (*DockerClient, error) {
 	client, err := dockercli.NewClientWithOpts(
 		dockercli.FromEnv,
@@ -40,6 +43,7 @@ func NewDockerClient(dockerOptions *DockerOptions) (*DockerClient, error) {
 	return &DockerClient{client, dockerOptions}, nil
 }
 
+// ImageBuild builds a Docker image from the given local repository path and tags it.
 func (d *DockerClient) ImageBuild(
 	registryOwner,
 	imageName,
@@ -55,29 +59,36 @@ func (d *DockerClient) ImageBuild(
 		imageTag,
 	)
 
+	// Create a tar archive of the local repository path.
 	tar, err := archive.TarWithOptions(localRepoPath, &archive.TarOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to create tar archive: %w", err)
 	}
+
+	// Define options for building the image.
 	buildOptions := types.ImageBuildOptions{
 		Dockerfile:  d.DockerOptions.Dockerfile,
 		Tags:        []string{registryNameWithTag},
 		Remove:      true,
 		ForceRemove: true,
 	}
+
 	log.Infof("Building image: %s", registryNameWithTag)
+	// Build the image
 	buildRes, err := d.Client.ImageBuild(context.Background(), tar, buildOptions)
 	if err != nil {
 		return fmt.Errorf("failed to build image: %w", err)
 	}
 	defer buildRes.Body.Close()
 
+	// Stream the build output to the console.
 	io.Copy(os.Stdout, buildRes.Body)
 
 	log.Infof("Image %s is built locally", registryNameWithTag)
 	return nil
 }
 
+// ImagePush pushes the image to the container registry.
 func (d *DockerClient) ImagePush(registryOwner, imageName, imageTag string) error {
 	containerRegistry := d.DockerOptions.ContainerRegistry
 	registryNameWithTag := fmt.Sprintf(
@@ -90,6 +101,7 @@ func (d *DockerClient) ImagePush(registryOwner, imageName, imageTag string) erro
 
 	registryPassword := d.DockerOptions.RegistryPassword
 
+	// Encode authentication configuration for the registry.
 	authConfig := registry.AuthConfig{
 		Username: registryOwner,
 		Password: registryPassword,
@@ -105,18 +117,21 @@ func (d *DockerClient) ImagePush(registryOwner, imageName, imageTag string) erro
 	}
 
 	log.Infof("Pushing image: %s", registryNameWithTag)
+	// Push the image to the registry.
 	pushRes, err := d.Client.ImagePush(context.Background(), registryNameWithTag, pushOptions)
 	if err != nil {
 		return fmt.Errorf("failed to push image: %w", err)
 	}
 	defer pushRes.Close()
 
+	// Stream the push output to the console.
 	io.Copy(os.Stdout, pushRes)
 
 	log.Infof("Image %s is pushed to the container registry", registryNameWithTag)
 	return nil
 }
 
+// ImageDelete deletes a Docker image from the local system and prunes dangling images.
 func (d *DockerClient) ImageDelete(registryOwner, imageName, imageTag string) error {
 	containerRegistry := d.DockerOptions.ContainerRegistry
 	registryNameWithTag := fmt.Sprintf(
@@ -133,11 +148,13 @@ func (d *DockerClient) ImageDelete(registryOwner, imageName, imageTag string) er
 	}
 
 	log.Infof("Deleting image: %s", registryNameWithTag)
+	// Remove the image.
 	_, err := d.Client.ImageRemove(context.Background(), registryNameWithTag, removeOptions)
 	if err != nil {
 		return fmt.Errorf("failed to delete image: %w", err)
 	}
 
+	// Prune dangling images to free up space.
 	if err := d.pruneDanglingImages(); err != nil {
 		return err
 	}
@@ -146,6 +163,7 @@ func (d *DockerClient) ImageDelete(registryOwner, imageName, imageTag string) er
 	return nil
 }
 
+// pruneDanglingImages removes dangling images from the local system to free up space.
 func (d *DockerClient) pruneDanglingImages() error {
 	// Set up filter to only target dangling images
 	// 'Dangling' images are those tagged with <none>
