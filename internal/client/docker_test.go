c1ca7e2437c350374b0fda84bf244f5838025378
Rui Wang <wruiwr86@gmail.com>
2024-08-14 11:41:11 +0200

    improve testing for docker

--- internal/client/docker_test.go
+++ internal/client/docker_test.go
@@ -12,6 +12,8 @@ var dockerTestCases = []struct {
 	registryOwner    string
 	imageTag         string
 	localRepoSrcPath string
+	repo             string
+	branch           string
 	dockerOptions    *DockerOptions
 }{
 	{
@@ -19,6 +21,8 @@ var dockerTestCases = []struct {
 		registryOwner:    "uib-ub",
 		imageTag:         "test",
 		localRepoSrcPath: os.Getenv("LOCAL_REPO_SRC"),
+		repo:             "uib-ub/uib-ub-monorepo",
+		branch:           "main",
 		dockerOptions: &DockerOptions{
 			ContainerRegistry: "ghcr.io",
 			RegistryPassword:  os.Getenv("GITHUB_TOKEN"),
@@ -27,55 +31,58 @@ var dockerTestCases = []struct {
 	},
 }
 
-func TestImageBuild(t *testing.T) {
+func TestImageLifecycle(t *testing.T) {
 	for i, tc := range dockerTestCases {
+		githubCli := NewGithubClient(tc.dockerOptions.RegistryPassword)
+		err := githubCli.DownloadGithubRepository(tc.localRepoSrcPath, tc.repo, tc.branch)
+		if err != nil {
+			t.Errorf("failed to download repository in test case %d: expected nil, got %v", i, err)
+		}
+
 		dockerClient, err := NewDockerClient(tc.dockerOptions)
 		if err != nil {
 			t.Errorf("failed to create docker client in test case %d: expected nil, got %v", i, err)
 		}
 
-		err = dockerClient.ImageBuild(
-			tc.registryOwner,
-			tc.imageName,
-			tc.imageTag,
-			tc.localRepoSrcPath,
-		)
-		if err != nil {
-			t.Errorf("failed to build image in test case %d: expected nil, got %v", i, err)
-		}
-	}
-}
-
-func TestImagePush(t *testing.T) {
-	for i, tc := range dockerTestCases {
-		dockerClient, err := NewDockerClient(tc.dockerOptions)
-		if err != nil {
-			t.Errorf("failed to create docker client in test case %d: expected nil, got %v", i, err)
-		}
-		err = dockerClient.ImagePush(tc.registryOwner, tc.imageName, tc.imageTag)
-		if err != nil {
-			t.Errorf("failed to push image in test case %d: expected nil, got %v", i, err)
-		}
-
-		t.Cleanup(func() {
-			githubCli := NewGithubClient(tc.dockerOptions.RegistryPassword)
-			err := githubCli.DeletePackageImage(context.Background(), tc.registryOwner, "container", tc.imageName, tc.imageTag)
+		t.Run("ImageBuild", func(t *testing.T) {
+			err = dockerClient.ImageBuild(
+				tc.registryOwner,
+				tc.imageName,
+				tc.imageTag,
+				tc.localRepoSrcPath,
+			)
 			if err != nil {
-				t.Errorf("failed to clean up by deleting image on Github packages in test case %d: expected nil, got %v", i, err)
+				t.Errorf("failed to build image in test case %d: expected nil, got %v", i, err)
 			}
 		})
-	}
-}
 
-func TestImageDelete(t *testing.T) {
-	for i, tc := range dockerTestCases {
-		dockerClient, err := NewDockerClient(tc.dockerOptions)
-		if err != nil {
-			t.Errorf("failed to create docker client in test case %d: expected nil, got %v", i, err)
-		}
-		err = dockerClient.ImageDelete(tc.registryOwner, tc.imageName, tc.imageTag)
-		if err != nil {
-			t.Errorf("failed to delete image in test case %d: expected nil, got %v", i, err)
-		}
+		t.Run("ImagePush", func(t *testing.T) {
+			err = dockerClient.ImagePush(tc.registryOwner, tc.imageName, tc.imageTag)
+			if err != nil {
+				t.Errorf("failed to push image in test case %d: expected nil, got %v", i, err)
+			}
+
+			t.Cleanup(func() {
+
+				err := githubCli.DeletePackageImage(context.Background(), tc.registryOwner, "container", tc.imageName, tc.imageTag)
+				if err != nil {
+					t.Errorf("failed to clean up by deleting image on Github packages in test case %d: expected nil, got %v", i, err)
+				}
+			})
+		})
+
+		t.Run("ImageDelete", func(t *testing.T) {
+			err = dockerClient.ImageDelete(tc.registryOwner, tc.imageName, tc.imageTag)
+			if err != nil {
+				t.Errorf("failed to delete image in test case %d: expected nil, got %v", i, err)
+			}
+		})
+
+		t.Cleanup(func() {
+			err := githubCli.DeleteLocalRepository(tc.localRepoSrcPath)
+			if err != nil {
+				t.Errorf("failed to clean up by deleting local repository in test case %d: expected nil, got %v", i, err)
+			}
+		})
 	}
 }
